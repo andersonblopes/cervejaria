903e147a945056064b3f656019a1f800e856193c
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-07-15 14:29:58 -0300

    Cadastro da cerveja iniciado.

--- src/main/java/com/lopes/cervejaria/controller/CervejasController.java
+++ src/main/java/com/lopes/cervejaria/controller/CervejasController.java
@@ -2,12 +2,21 @@ package com.lopes.cervejaria.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lopes.cervejaria.model.Cerveja;
 
 @Controller
 public class CervejasController {
 
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/cadastro-cerveja";
+	}
+
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println(">>> Cerveja: " + cerveja.getNome() + " - " + cerveja.getSku());
 		return "cerveja/cadastro-cerveja";
 	}
 
