89be645a3aa0f95738938d6c6981bde77daf5f3d
andersonblopes <anderson.lopes.2010@gmail.com>
2017-12-18 21:31:52 -0300

    mapeia entidade Cerveja

--- src/main/java/com/lopes/cervejaria/model/Cerveja.java
+++ src/main/java/com/lopes/cervejaria/model/Cerveja.java
@@ -1,10 +1,34 @@
 package com.lopes.cervejaria.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-public class Cerveja {
+import com.lopes.cervejaria.model.enumeration.Origem;
+import com.lopes.cervejaria.model.enumeration.Sabor;
+
+@Entity
+@Table(name = "cerveja")
+public class Cerveja implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@NotBlank(message = "SKU é obrigatório!")
 	private String sku;
@@ -14,6 +38,34 @@ public class Cerveja {
 
 	@Size(min = 1, max = 50, message = "A descrição deve ter entre 1 a 50 caracteres!")
 	private String descricao;
+
+	private BigDecimal valor;
+
+	@Column(name = "teor_alcolico")
+	private BigDecimal teorAlcolico;
+
+	private BigDecimal comissao;
+
+	@Column(name = "quantidade_estoque")
+	private Integer quantidadeEstoque;
+
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+
+	@ManyToOne
+	@JoinColumn(name = "id_estilo")
+	private Estilo estilo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getSku() {
 		return sku;
@@ -37,6 +89,91 @@ public class Cerveja {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getTeorAlcolico() {
+		return teorAlcolico;
+	}
+
+	public void setTeorAlcolico(BigDecimal teorAlcolico) {
+		this.teorAlcolico = teorAlcolico;
+	}
+
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Origem getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+
+	public Sabor getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+
+	public Estilo getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
---
+++ src/main/java/com/lopes/cervejaria/model/enumeration/Origem.java
@@ -0,0 +1,17 @@
+package com.lopes.cervejaria.model.enumeration;
+
+public enum Origem {
+
+	NACIONAL("Nacional"), INTERNACIONAL("Internacional");
+
+	private String descricao;
+
+	Origem(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
